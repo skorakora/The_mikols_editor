dc3cea95da034d374bd395747c695b313d34a665
skorakora <skorakora@wp.pl>
2020-07-06 00:20:05 +0200

    Menu główne - dodanie podstawowych przycisków

---
+++ Assets/Scripts/Main_menu/Editor_canvas.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Editor_canvas : MonoBehaviour
+{
+    public string path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DirectoryInfo dir = new DirectoryInfo(path);
+        if (dir.Exists)
+        {
+            foreach (var file in dir.GetFiles())
+            {
+                if (file.Name.Contains(".scn"))
+                {
+                    string[] scnname;
+                    scnname = file.Name.Split('.');
+                    print("sceneria:  "+scnname[0]);
+                }
+            }
+        }
+        else
+        {
+            print("ACHTUNG ACHTUNG ERROR.EXE, Nie znaleziono folderu");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Main_menu/Main_menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Main_menu : MonoBehaviour
+{
+    public GameObject Main_buttons;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        Main_buttons.SetActive(true);
+    }
+    public void Aplicationquit()
+    {
+        Application.Quit();
+    }
+
+    public void Editor()
+    {
+        Main_buttons.SetActive(false);
+    }
+}
