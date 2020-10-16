a4c8dbba03e1654ea900b34512d91eccf1586e43
skorakora <skorski.6a.20.2012@wp.pl>
2020-10-16 03:26:23 +0100

    Dodanie generatora toru

---
+++ Assets/Scripts/SCN_Editor/Track/Track_gen.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Track_gen : MonoBehaviour
+{
+    public Vector3 punkt1;
+    public Vector3 punkt2;
+    public Vector3 punkt3;
+    public Vector3 punkt4;
+
+    List<Vector3> points;
+
+    void Start()
+    {
+        gen_track(punkt1, punkt2, punkt3, punkt4);
+
+    }
+
+    // p1 p4 punkty początku i końca toru   p2 p3 punkty kontrolne krzywej beziera.
+    public void gen_track(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4) //to jest główna funkcja.
+    {
+        p1 = points[0];
+        p2 = points[1];
+        p3 = points[2];
+        p4 = points[3];
+
+    }
+
+
+
+
+
+
+    public  Vector3 EvaluateQuadratic(Vector3 a, Vector3 b, Vector3 c, float t)
+    {
+        Vector3 p0 = Vector3.Lerp(a, b, t);
+        Vector3 p1 = Vector3.Lerp(b, c, t);
+        return Vector3.Lerp(p0, p1, t);
+    }
+
+    public  Vector3 EvaluateCubic(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t)
+    {
+        Vector3 p0 = EvaluateQuadratic(a, b, c, t);
+        Vector3 p1 = EvaluateQuadratic(b, c, d, t);
+        return Vector3.Lerp(p0, p1, t);
+    }
+
+    public Vector2[] CalculateEvenlySpacedPoints(float spacing, float resolution = 1)
+    {
+        List<Vector2> evenlySpacedPoints = new List<Vector2>();
+        evenlySpacedPoints.Add(points[0]);
+        Vector2 previousPoint = points[0];
+        float dstSinceLastEvenPoint = 0;
+
+        for (int segmentIndex = 0; segmentIndex < NumSegments; segmentIndex++)
+        {
+            Vector2[] p = GetPointsInSegment(segmentIndex);
+            float controlNetLength = Vector2.Distance(p[0], p[1]) + Vector2.Distance(p[1], p[2]) + Vector2.Distance(p[2], p[3]);
+            float estimatedCurveLength = Vector2.Distance(p[0], p[3]) + controlNetLength / 2f;
+            int divisions = Mathf.CeilToInt(estimatedCurveLength * resolution * 10);
+            float t = 0;
+            while (t <= 1)
+            {
+                t += 1f / divisions;
+                Vector2 pointOnCurve = EvaluateCubic(p[0], p[1], p[2], p[3], t);
+                dstSinceLastEvenPoint += Vector2.Distance(previousPoint, pointOnCurve);
+
+                while (dstSinceLastEvenPoint >= spacing)
+                {
+                    float overshootDst = dstSinceLastEvenPoint - spacing;
+                    Vector2 newEvenlySpacedPoint = pointOnCurve + (previousPoint - pointOnCurve).normalized * overshootDst;
+                    evenlySpacedPoints.Add(newEvenlySpacedPoint);
+                    dstSinceLastEvenPoint = overshootDst;
+                    previousPoint = newEvenlySpacedPoint;
+                }
+
+                previousPoint = pointOnCurve;
+            }
+        }
+
+        return evenlySpacedPoints.ToArray();
+    }
+
+    public Vector2[] GetPointsInSegment(int i)
+    {
+        return new Vector2[] { points[i * 3], points[i * 3 + 1], points[i * 3 + 2], points[LoopIndex(i * 3 + 3)] };
+    }
+
+
+    int LoopIndex(int i)
+    {
+        return (i + points.Count) % points.Count;
+    }
+
+    public int NumSegments
+    {
+        get
+        {
+            return points.Count / 3;
+        }
+    }
+}
