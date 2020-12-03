236e861f9a19654ef65b7abf5837a0bf49b808ea
skorakora <skorski.6a.20.2012@wp.pl>
2020-12-03 01:18:01 +0000

    Konwersja sn_utils z C++ na C#
    Przepisałem jeden z krytyczny do ładowania modeli metod z kodu maszyny na C#

---
+++ Assets/Scripts/SCN_Editor/E3D/E3D_loader.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+public class E3D_loader : MonoBehaviour
+{
+    Sn_utils sn_utils = new Sn_utils();
+
+    public string path = "C:\\Program Files (x86)\\Maszyna\\dynamic\\pkp\\en57_v1\\en57-840s.e3d";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Load(path);
+    }
+
+    public void Load(string _path)
+    {
+        FileStream file = new FileStream(_path, FileMode.Open, FileAccess.Read);
+        Debug.Log(sn_utils.ld_uint16(file));
+    }
+
+}
---
+++ Assets/Scripts/SCN_Editor/E3D/sn_utils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Sn_utils
+{
+    // deserialize little endian uint16
+    public UInt16 ld_uint16 (FileStream s)
+    {
+        byte[] buf = new byte[2];
+        s.Read(buf,0,2);
+        return BitConverter.ToUInt16(buf, 0);
+    }
+
+    // deserialize little endian uint32
+    public UInt32 ld_uint32(FileStream s)
+    {
+        byte[] buf = new byte[4];
+        s.Read(buf, 0, 4);
+        return BitConverter.ToUInt32(buf, 0);
+    }
+
+    // deserialize little endian int32
+    public Int32 ld_int32(FileStream s)
+    {
+        byte[] buf = new byte[4];
+        s.Read(buf, 0, 4);
+        return BitConverter.ToInt32(buf, 0);
+    }
+
+    // deserialize little endian float32
+    public float ld_float32(FileStream s)
+    {
+        byte[] buf = new byte[4];
+        s.Read(buf, 0, 4);
+        return BitConverter.ToSingle(buf, 0);
+    }
+
+    // deserialize little endian float64
+    public double ld_float64(FileStream s)
+    {
+        byte[] buf = new byte[8];
+        s.Read(buf, 0, 8);
+        return BitConverter.ToDouble(buf, 0);
+    }
+
+
+}
+ 
