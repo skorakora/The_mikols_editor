503c0cfe45f3e0cecc3d5af5855b5b3e597cad8e
DESKTOP-992HB7U\komp <skorski.6a.20.2012@wp.pl>
2021-08-14 00:42:41 +0200

    Added terrain class W tej klasie siedzi wszystko związane z terenem

---
+++ Assets/Scripts/SCN_Editor/Terrain.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terrain : MonoBehaviour
+{
+    string terrain_name;
+    List<Vector3> vertices = new List<Vector3>();
+    List<Vector3> normals = new List<Vector3>();
+    List<Vector2> UV = new List<Vector2>();
+    List<int> triangles = new List<int>();
+    int triangle_counter = 0;
+
+    public Terrain(string _name)
+    {
+        terrain_name = _name;
+        triangle_counter = 0;
+    }
+
+    public void AddVertex(Vector3 vertex,Vector3 normal,Vector2 _UV)
+    {
+        vertices.Add(vertex);
+        normals.Add(normal);
+        UV.Add(_UV);
+        triangles.Add(triangle_counter);
+        triangle_counter++;
+    }
+
+    public void SetMaterial()
+    {
+
+    }
+
+    public void SetTexture(Texture2D texture)
+    {
+
+    }
+
+    public Mesh GetMesh()
+    {
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices.ToArray();
+        mesh.normals = normals.ToArray();
+        mesh.uv = UV.ToArray();
+        mesh.triangles = triangles.ToArray();
+        return mesh;
+
+    }
+
+    public string GetName()
+    {
+        return terrain_name;
+    }
+
+
+}
