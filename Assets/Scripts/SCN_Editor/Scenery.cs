3b6f29c2b40a463609c6a2849ae25de1e90cd930
DESKTOP-992HB7U\komp <skorski.6a.20.2012@wp.pl>
2021-08-14 00:41:25 +0200

    Added scenery class W tej klasie siedzi cała sceneria, tutaj będą wszystkie metody do umieszczania, czy usuwania obiektów na mapie

---
+++ Assets/Scripts/SCN_Editor/Scenery.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenery : MonoBehaviour
+{
+    //--------------------------------------------------CONSTRUCTOR--------------------------------------------------------
+    Dictionary<string, GameObject> Map = new Dictionary<string, GameObject>();//List of categories on scenery (objects avaible to spawn)
+
+    public Scenery()
+    {
+        GameObject SCN_root = new GameObject("Scenery");
+        //elementy scenerii
+        GameObject obj = new GameObject();
+        Map.Add("terrain", AddCategory("terrain",SCN_root));
+    }
+
+    GameObject AddCategory(string name,GameObject Root)//porządkuje obiekty w drzewku by nie było syfu
+    {
+        GameObject obj = new GameObject(name);
+        obj.transform.parent = Root.transform;
+        return obj;
+    }
+
+
+    //-----------------------------------------------------METHODS---------------------------------------------------------
+
+    public void AddMesh(Mesh mesh,float range_max, float range_min,string name)
+    {
+        GameObject ROOT = Map["terrain"];
+        GameObject obj = new GameObject(name);
+        obj.transform.parent = ROOT.transform;
+        MeshFilter meshFilter = obj.AddComponent<MeshFilter>();
+        obj.AddComponent<MeshRenderer>();
+        meshFilter.mesh = mesh;
+    }
+}
+
