f0599268b225acbcfe1661fbde1b88d688d50f12
DESKTOP-992HB7U\komp <skorski.6a.20.2012@wp.pl>
2021-08-14 00:03:57 +0200

    Revert "Added simple slider for camera speed velocity"
    This reverts commit 8bbd625b5d6a4a83e016a08977b7c511abc4b24a.

--- Assets/Generator/scripts/camera_movement.cs
+++ Assets/Generator/scripts/camera_movement.cs
@@ -1,55 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class camera_movement : MonoBehaviour
 {
     // Co każdy frame
-    private float vel;
     void Update()
     {
-        
         if (Input.GetKey("w"))
         {
-            vel = (float) GameObject.Find("VelSlider").GetComponent<Slider>().value; //Pobieramy wartosc slidera po nazwie 
-            transform.Translate(0, 0, 1*vel); //mnożymy przez wartość slidera (min. 0.01 max 2)
-            //krótka adnotacja: pobranie wartości tylko wtedy gdy klikniemy guzik wpłynie na optymalizację "bez niczego"
+            transform.Translate(0, 0, 1);
         }
         if (Input.GetKey("s"))
         {
-            vel = (float)GameObject.Find("VelSlider").GetComponent<Slider>().value;
-            transform.Translate(0, 0, -1*vel);
+            transform.Translate(0, 0, -1);
         }
         if (Input.GetKey("a"))
         {
-            vel = (float)GameObject.Find("VelSlider").GetComponent<Slider>().value;
-            transform.Translate(-1*vel, 0, 0);
+            transform.Translate(-1, 0, 0);
         }
         if (Input.GetKey("d"))
         {
-            vel = (float)GameObject.Find("VelSlider").GetComponent<Slider>().value;
-            transform.Translate(1*vel, 0, 0);
+            transform.Translate(1, 0, 0);
         }
         if (Input.GetKey("q"))
         {
-            vel = (float)GameObject.Find("VelSlider").GetComponent<Slider>().value;
-            transform.Translate(0, -1*vel, 0);
+            transform.Translate(0, -1, 0);
         }
         if (Input.GetKey("e"))
         {
-            vel = (float)GameObject.Find("VelSlider").GetComponent<Slider>().value;
-            transform.Translate(0, 1*vel, 0);
+            transform.Translate(0, 1, 0);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            vel = (float)GameObject.Find("VelSlider").GetComponent<Slider>().value;
-            transform.Rotate (0, -1*vel, 0);
+            transform.Rotate (0, -1, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            vel = (float)GameObject.Find("VelSlider").GetComponent<Slider>().value;
-            transform.Rotate (0, 1*vel, 0);
+            transform.Rotate (0, 1, 0);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
