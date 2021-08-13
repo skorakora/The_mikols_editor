f00b7d4243a595be13c122e1a6971fc96fa4d48d
DESKTOP-992HB7U\komp <skorski.6a.20.2012@wp.pl>
2021-08-14 00:35:59 +0200

    Added back default camera Takie eksperymenty jak ten z kamerą mogą zostać wysłane TYLKO jak są w pełni sprawdzone, a nie że wysyłasz niedziałającą kamerę na master i tak zostawiasz - zabranie dostępu do repo

---
+++ Assets/Scripts/SCN_Editor/Camera/camera_movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_movement : MonoBehaviour
+{
+    // Co każdy frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(0, 0, 1);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(0, 0, -1);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-1, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(1, 0, 0);
+        }
+        if (Input.GetKey("q"))
+        {
+            transform.Translate(0, -1, 0);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.Translate(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate (0, -1, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate (0, 1, 0);
+        }
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+    }
+}
